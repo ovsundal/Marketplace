6604df9d81cfa98215e627ee6305817f572c6041
osund <osundal83@gmail.com>
2023-03-02 21:47:13 +0100

    add ClassifiedAd entity and UserId, ClassifiedAdId value objects

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,21 @@
+﻿namespace Marketplace.Domain;
+
+public class ClassifiedAd
+{
+    public Guid Id { get; }
+    private UserId _ownerId;
+
+    public ClassifiedAd(Guid id, UserId ownerId)
+    {
+        Id = id; // validation check for ad id moved to valueobject ClassifiedAdId
+        _ownerId = ownerId; // validation check for ownerId moved to valueobject UserId
+    }
+
+    public void SetTitle(string title) => _title = title;
+    public void UpdateText(string text) => _text = text;
+    public void UpdatePrice(decimal price) => _price = price;
+
+    private string _title;
+    private string _text;
+    private decimal _price;
+}
---
+++ Marketplace.Domain/UserId.cs
@@ -0,0 +1,16 @@
+﻿namespace Marketplace.Domain;
+
+public class UserId
+{
+    private readonly Guid _value;
+
+    public UserId(Guid value)
+    {
+        if (value == default)
+        {
+            throw new ArgumentException(nameof(value), "User id cannot be empty");
+        }
+
+        _value = value;
+    }
+}
