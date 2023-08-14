734fa029040b6596e2513bf6243a2f9413c4aacb
OVSU <ovsu@equinor.com>
2023-08-14 14:14:58 +0200

    add missing price code

--- Marketplace.Domain/Price.cs
+++ Marketplace.Domain/Price.cs
@@ -2,11 +2,21 @@ namespace Marketplace.Domain;
 
 public record Price : Money
 {
-    public Price(Money amount) : base(amount)
+    private Price(decimal amount, string currencyCode, ICurrencyLookup currencyLookup)
+        : base(amount, currencyCode, currencyLookup)
     {
-        // if (amount < 0)
-        // {
-        //     throw new ArgumentException("Price cannot be negative", nameof(amount));
-        // }
+        if (amount < 0)
+            throw new ArgumentException(
+                "Price cannot be negative",
+                nameof(amount));
     }
+
+    // internal Price(decimal amount, string currencyCode)
+    //     : base(amount, new Currency{CurrencyCode = currencyCode})
+    // {
+    // }
+
+    public new static Price FromDecimal(decimal amount, string currency,
+        ICurrencyLookup currencyLookup) =>
+        new Price(amount, currency, currencyLookup);
 }
